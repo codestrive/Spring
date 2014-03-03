a6e9a5cd96f636c291e5a3a7c50b986307b6156e
Koushik Ghosh <koushik08oct@gmail.com>
2014-03-03 10:36:09 +0530

    modified client class

--- SpringBasics/src/com/codestrive/tightCouple/Client.java
+++ SpringBasics/src/com/codestrive/tightCouple/Client.java
@@ -1,6 +1,5 @@
 package com.codestrive.tightCouple;
 
-import java.util.Scanner;
 
 public class Client {
 	
@@ -8,16 +7,6 @@ public class Client {
 		PaymentService payment = new PaymentService();
 		payment.pay();
 	}
-	private static String getUserInput(){
-		
-		String accountTypeOption = "";
-		Scanner userInput =  new Scanner(System.in);
-		System.out.println("Which account(S/C/L) do you want to use");
-		if(userInput.hasNextLine()){
-			accountTypeOption = userInput.nextLine();
-		}
-		userInput.close();
-		return accountTypeOption;
-	}
+	
 	
 }
