e187a3a87c2eab20e66f972004fd583d369d7e2f
Koushik Ghosh <koushik08oct@gmail.com>
2014-05-05 22:25:29 +0530

    SpringMVC Demo Project
    Demonstration of Spring MVC

--- springmvc/src/codestirve/springMvc/controller/HomeController.java
+++ springmvc/src/codestirve/springMvc/controller/HomeController.java
@@ -1,11 +1,15 @@
-package com.koushik.test;
+package codestirve.springMvc.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.sql.DataSource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
+	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
@@ -24,6 +29,7 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
+		
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
